d07434b6a4a3740ec5d76f85eb1661b7e0440e68
unknown <brianwagako@gmail.com>
2023-02-08 12:24:15 +0300

    changeprojectphoto

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import portfolio from '../public/projects/photosite.png'
+import portfolio from '../public/projects/portfolio.png'
 
 const Projects = () => {
   return (
