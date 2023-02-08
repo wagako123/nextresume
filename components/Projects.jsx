74875336383c40fba474ab75f817e8a67c69fece
unknown <brianwagako@gmail.com>
2023-02-08 11:37:04 +0300

    pngtest2

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import portfolio from '../public/projects/portfolio.PNG'
+import portfolio from '../public/projects/portfolio2.PNG'
 
 const Projects = () => {
   return (
