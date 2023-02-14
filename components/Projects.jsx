fc44ea538492a14fbd44847243c4952097416817
unknown <brianwagako@gmail.com>
2023-02-14 12:15:04 +0300

    removeprojectimage2

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
             <h2 className='py-4'>What I've Built</h2>
             <div className='grid md:grid-cols-2 gap-8'>
                 <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#123456] to-[#545454] '>
-                    <Image className='rounded-xl group-hover:opacity-10' src={portfolio} alt='/' />
+                    <Image className='rounded-xl group-hover:opacity-10' src='/' alt='/' />
                     <div className='hidden group-hover:block absolute top-[50%] lefft-[50%] translate-x-[-50%] translate-y-[-50%]'>
                       <h3 className='text-2xl text-white tracking-wider  text-center'>Photo portfolio site</h3>
                       <p className='pb-4 pt-2 text-white text-center'>Next js</p>
