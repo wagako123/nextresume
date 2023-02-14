3bf1be1ff10a476e76b8dc97386655ebb5ae1c5a
unknown <brianwagako@gmail.com>
2023-02-14 12:16:53 +0300

    removeprojectimage3

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import portfolio from '../public/projects/logowbg.png'
 
 const Projects = () => {
   return (
