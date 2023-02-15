0a84f075aa70100edcab05cbbaa0663fbffc5d6f
unknown <brianwagako@gmail.com>
2023-02-15 09:05:09 +0300

    navbackground

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,12 +4,28 @@ import React, {useState, useEffect}from 'react'
 import {AiOutlineClose, AiOutlineMail, AiOutlineMenu} from 'react-icons/ai'
 import {FaGithub, FaLinkedinIn} from 'react-icons/fa'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
+import { useRouter } from 'next/router'
 
 
 const Navbar = () => {
 
   const [nav, setNav]=useState(false);
-  const [shadow, setShadow]= useState(false)
+  const [shadow, setShadow]= useState(false);
+  const [navBg, setNavBg]= useState('#ecf0f3');
+  const [linkColor, setLinkColor] = useState('#1f2937');
+  const router=useRouter;
+
+  useEffect(()=>{
+    if(
+      router.asPath ==='/Photo'
+    ){
+      setNavBg('transparent')
+      setLinkColor('#ecf0f3')
+    }else{
+      setNavBg('#ecf0f3')
+      setLinkColor('#1f2937')
+    }
+  },[router])
 
   const handleNav= ()=>{
    setNav(!nav)
@@ -27,11 +43,12 @@ const Navbar = () => {
   },[])
 
   return (
-    <div className={ shadow? 'fixed w-full h-20 shadow-xl z-{100}':'fixed w-full h-20 z-{100}'}>
+    <div style={{backgroundColor: `${navBg}`}}
+     className={ shadow? 'fixed w-full h-20 shadow-xl z-{100}':'fixed w-full h-20 z-{100}'}>
       <div className='flex justify-between items-center w-full h-full px-2 xl:px-16'>
         <Image src='/../public/assets/correctbg.png' alt='/' width='80' height='30'/>
         <div>
-          <ul className='hidden md:flex'>
+          <ul style={{color:`${linkColor}`}} className='hidden md:flex'>
             <Link href='/'>
               <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
             </Link>
@@ -58,7 +75,9 @@ const Navbar = () => {
         <div className={nav ?'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500': 'fixed left-[-100%]  p-10 ease-in duration-500 '}>
           <div>
             <div className='flex w-full items-center justify-between'>
-              <Image src='/../public/assets/correctbg.png' alt='/' width='150' height='80'/>
+              <Link href='/'>
+              <Image onClick={()=>setNav(false)} src='/../public/assets/correctbg.png' alt='/' width='150' height='80'/>
+              </Link>
               <div onClick={handleNav} className=' rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
                 <AiOutlineClose />
               </div>
