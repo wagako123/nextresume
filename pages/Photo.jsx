1ef3e9efb1dfb5b5701f768345a654a89e23aac6
unknown <brianwagako@gmail.com>
2023-02-21 12:35:28 +0300

    pictureremoval

--- pages/Photo.jsx
+++ pages/Photo.jsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import React from 'react'
-import photoImg from '../public/projects/portfolio.JPG'
 
 const Photo = () => {
   return (
     <div className='w-full'>
       <div className='w-screen  h-[30vh] lg:w-[40vh] relative'>
+        <div className='absolute top-0 left-0 w-full h-[30vh] lg:'>
+
+        </div>
         
 
       </div>
